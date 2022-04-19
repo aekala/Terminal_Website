f72256371e7f1e052e422d041e03a965fa3f8d5a
Leo Kodish <lkkodish@gmail.com>
2022-04-19 00:24:45 -0700

    removed unused imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./styles/index.css";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 import { createRoot } from "react-dom/client";
