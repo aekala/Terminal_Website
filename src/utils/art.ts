57e96e6869d5001863ccb0d7c0fd525436dde6eb
Leo Kodish <lkkodish@gmail.com>
2022-04-18 22:25:15 -0700

    changing from tiny text

--- src/utils/art.ts
+++ src/utils/art.ts
@@ -77,7 +77,7 @@ export const ghost = `
 </pre>`;
 
 export const banner = `
-<pre style="color: var(--color-border);" class="text-tiny tablet:text-sm laptop:text-xl">
+<pre style="color: var(--color-border);" class="text-xs tablet:text-sm laptop:text-xl">
 
 ██╗     ██╗  ██╗     ██████╗ ███████╗██╗   ██╗
 ██║     ██║ ██╔╝     ██╔══██╗██╔════╝██║   ██║
